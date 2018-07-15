405703487585ed76e586f41d72fb8aef53012d27
gentjan kolicaj <gentjankolicaj@yahoo.com>
2018-07-15 14:21:26 +0200

    Updated schema script.

--- MyManager/target/classes/com/mymanager/scripts/MyManager_mysql.sql
+++ MyManager/target/classes/com/mymanager/scripts/MyManager_mysql.sql
@@ -459,7 +459,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `mymanager`.`payment_type` ;
 
 CREATE TABLE IF NOT EXISTS `mymanager`.`payment_type` (
-  `payment_type` VARCHAR(10) NOT NULL,
+  `payment_type` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`payment_type`))
 ENGINE = InnoDB;
 
@@ -737,11 +737,50 @@ CREATE TABLE IF NOT EXISTS `mymanager`.`employee_additional_history` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Initial setup
+-- -----------------------------------------------------
 INSERT INTO `mymanager`. `user_type` (user_type) VALUES ('ADMIN');
 INSERT INTO `mymanager`.  `user_type` (user_type) VALUES ('HR');
 INSERT INTO  `mymanager`.`user_type` (user_type) VALUES ('ASSISTANT');
 INSERT INTO `mymanager`. `user_type` (user_type) VALUES ('MANAGER');
 INSERT INTO `mymanager`. `user_type` (user_type) VALUES ('FINANCE');
+
+
+
+-- -----------------------------------------------------
+-- Admin user to login initially on application,change password and username after first login
+-- -----------------------------------------------------
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("admin", "ADMIN", "Administrator", "Last Name", "admin",
+					curdate(), "Tirane", 'M', "READ", "Administrator",now(), "Administrator", now());
+
+
+-- -----------------------------------------------------
+-- Initial users,for testing purposess,to be deleted after testing
+-- -----------------------------------------------------
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("1", "ADMIN", "Test User ADMIN", "Last Name", "1",
+					curdate(), "Tirane", 'M', "READ", "Test User ADMIN",now(), "Test User ADMIN", now());
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("2", "HR", "Test User HR", "Last Name", "2",
+					curdate(), "Tirane", 'M', "READ", "Test User HR", now(),"Test User HR", now());
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("3", "ADMIN", "Test User FINANCE", "Last Name", "3",
+					curdate(), "Tirane", 'M', "READ", "Test User FINANCE", now(),"Test User FINANCE",now());
+										
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("4", "ADMIN", "Test User MANAGER", "Last Name", "4",
+					curdate(), "Tirane", 'M', "READ", "Test User MANAGER", now(),"Test User MANAGER", now());
+					
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("5", "ADMIN", "Test User ASSISTANT", "Last Name", "5",
+					curdate(), "Tirane", 'M', "READ", "Test User ASSISTANT", now(),"Test User ASSISTANT", now());
+					
+					
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- MyManager/target/classes/com/mymanager/scripts/MyManager_postgresql.sql
+++ MyManager/target/classes/com/mymanager/scripts/MyManager_postgresql.sql
@@ -348,7 +348,7 @@ CREATE TABLE IF NOT EXISTS   mymanager  .  user_adress_history   (
 -- Table   mymanager  .  payment_type  
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS   mymanager  .  payment_type   (
-    payment_type   VARCHAR(10) NOT NULL,
+    payment_type   VARCHAR(100) NOT NULL,
   PRIMARY KEY (  payment_type  )) ;
 
 
@@ -573,4 +573,38 @@ INSERT INTO  mymanager.user_type (user_type) VALUES ('HR');
 INSERT INTO  mymanager.user_type (user_type) VALUES ('ASSISTANT');
 INSERT INTO  mymanager.user_type (user_type) VALUES ('MANAGER');
 INSERT INTO  mymanager.user_type (user_type) VALUES ('FINANCE');
+
+
+
+-- -----------------------------------------------------
+-- Admin user to login initially on application,change password and username after first login
+-- -----------------------------------------------------
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("admin", "ADMIN", "Administrator", "Last Name", "admin",
+					curdate(), "Tirane", 'M', "READ", "Administrator",now(), "Administrator", now());
+
+
+-- -----------------------------------------------------
+-- Initial users,for testing purposess,to be deleted after testing
+-- -----------------------------------------------------
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("1", "ADMIN", "Test User ADMIN", "Last Name", "1",
+					curdate(), "Tirane", 'M', "READ", "Test User ADMIN",now(), "Test User ADMIN", now());
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("2", "HR", "Test User HR", "Last Name", "2",
+					curdate(), "Tirane", 'M', "READ", "Test User HR", now(),"Test User HR", now());
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("3", "ADMIN", "Test User FINANCE", "Last Name", "3",
+					curdate(), "Tirane", 'M', "READ", "Test User FINANCE", now(),"Test User FINANCE",now());
+										
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("4", "ADMIN", "Test User MANAGER", "Last Name", "4",
+					curdate(), "Tirane", 'M', "READ", "Test User MANAGER", now(),"Test User MANAGER", now());
+					
+					
+INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
+  gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("5", "ADMIN", "Test User ASSISTANT", "Last Name", "5",
+					curdate(), "Tirane", 'M', "READ", "Test User ASSISTANT", now(),"Test User ASSISTANT", now());
 
