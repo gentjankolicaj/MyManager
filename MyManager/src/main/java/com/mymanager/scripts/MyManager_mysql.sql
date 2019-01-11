a84c28edda7e2e87a233f36591db50eb11267536
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-11 11:26:53 +0100

    updated sql scripts.

--- MyManager/src/main/java/com/mymanager/scripts/MyManager_mysql.sql
+++ MyManager/src/main/java/com/mymanager/scripts/MyManager_mysql.sql
@@ -828,7 +828,7 @@ INSERT INTO `mymanager`.`file_types` (file_type) VALUES ("rtf");
 -- -----------------------------------------------------
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("admin", "ADMIN", "Administrator", "Last Name", "admin",
-					curdate(), "Tirane", 'M', "READ", "Administrator",now(), "Administrator", now());
+					curdate(), "Tirane", 'M', " READ", "Administrator",now(), "Administrator", now());
 
 
 -- -----------------------------------------------------
@@ -836,24 +836,24 @@ INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`pass
 -- -----------------------------------------------------
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("1", "ADMIN", "Test User ADMIN", "Last Name", "1",
-					curdate(), "Tirane", 'M', "READ", "Test User ADMIN",now(), "Test User ADMIN", now());
+					curdate(), "Tirane", 'M', " READ", "Test User ADMIN",now(), "Test User ADMIN", now());
 					
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("2", "HR", "Test User HR", "Last Name", "2",
-					curdate(), "Tirane", 'M', "READ", "Test User HR", now(),"Test User HR", now());
+					curdate(), "Tirane", 'M', " READ", "Test User HR", now(),"Test User HR", now());
 					
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("3", "FINANCE", "Test User FINANCE", "Last Name", "3",
-					curdate(), "Tirane", 'M', "READ", "Test User FINANCE", now(),"Test User FINANCE",now());
+					curdate(), "Tirane", 'M', " READ", "Test User FINANCE", now(),"Test User FINANCE",now());
 										
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("4", "MANAGER", "Test User MANAGER", "Last Name", "4",
-					curdate(), "Tirane", 'M', "READ", "Test User MANAGER", now(),"Test User MANAGER", now());
+					curdate(), "Tirane", 'M', " READ", "Test User MANAGER", now(),"Test User MANAGER", now());
 					
 					
 INSERT INTO `mymanager`. `users` (  user_id,user_type,first_name,last_name,`password`,birthday,birthplace,
   gender,rights,created_by,created_date,updated_by,updated_date) VALUES ("5", "ADMIN", "Test User ASSISTANT", "Last Name", "5",
-					curdate(), "Tirane", 'M', "READ", "Test User ASSISTANT", now(),"Test User ASSISTANT", now());
+					curdate(), "Tirane", 'M', " READ", "Test User ASSISTANT", now(),"Test User ASSISTANT", now());
                     
                     
                     
