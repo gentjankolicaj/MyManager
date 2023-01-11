9f7cfdfc236cb3cb8a1aa64cce81f54e94a586a0
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2023-01-11 22:55:28 +0100

    Changes related to readme.

--- desktop/src/main/java/io/gentjankolicaj/app/mymanager/desktop/view/about/AboutDialog.java
+++ desktop/src/main/java/io/gentjankolicaj/app/mymanager/desktop/view/about/AboutDialog.java
@@ -8,34 +8,32 @@ import java.awt.*;
 
 public class AboutDialog extends JDialog {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 990588574724534884L;
-	private final JPanel contentPanel = new JPanel();
+    /**
+     *
+     */
+    private static final long serialVersionUID = 990588574724534884L;
+    private final JPanel contentPanel = new JPanel();
 
-	/**
-	 * Create the dialog.
-	 */
-	public AboutDialog() {
-		setTitle("About");
-		setIconImage(IconUtils.getImage("icons/icons_24x24/icons8-admin-2.png"));
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 622, 332);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		{
-			JTextArea txtrAboutAuthorGentjan = new JTextArea();
-			txtrAboutAuthorGentjan.setEditable(false);
-			txtrAboutAuthorGentjan.setLineWrap(true);
-			txtrAboutAuthorGentjan.setText(
-					"Author : Gentjan Koli\u00E7aj \r\nEmail : gentjankolicaj@gmail.com\r\nGithub : https://github.com/gentjankolicaj\r\n\r\nAbout Icons : \r\nIcons are property of https://icons8.com/. \r\nThey distribute icons under the license called Creative Commons Attribution-NoDerivs 3.0 Unported.\r\nA special thanks to them.\r\nAlso check the site for more awesome icons.");
-			txtrAboutAuthorGentjan.setBackground(UIManager.getColor("Button.background"));
-			txtrAboutAuthorGentjan.setBounds(10, 11, 586, 215);
-			contentPanel.add(txtrAboutAuthorGentjan);
-		}
-	}
+    /**
+     * Create the dialog.
+     */
+    public AboutDialog() {
+        setTitle("About");
+        setIconImage(IconUtils.getImage("icons/icons_24x24/icons8-admin-2.png"));
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 622, 332);
+        getContentPane().setLayout(new BorderLayout());
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        getContentPane().add(contentPanel, BorderLayout.CENTER);
+        contentPanel.setLayout(null);
+        JTextArea txtrAboutAuthorGentjan = new JTextArea();
+        txtrAboutAuthorGentjan.setEditable(false);
+        txtrAboutAuthorGentjan.setLineWrap(true);
+        txtrAboutAuthorGentjan.setText(
+                "Author : Gentjan Kolicaj \r\nGithub : https://github.com/gentjankolicaj\r\n\r\nAbout Icons : \r\nIcons are property of https://icons8.com/. \r\nThey distribute icons under the license called Creative Commons Attribution-NoDerivs 3.0 Unported.\r\nA special thanks to them.\r\nAlso check the site for more awesome icons.");
+        txtrAboutAuthorGentjan.setBackground(UIManager.getColor("Button.background"));
+        txtrAboutAuthorGentjan.setBounds(10, 11, 586, 215);
+        contentPanel.add(txtrAboutAuthorGentjan);
+    }
 
 }
